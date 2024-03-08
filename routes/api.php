cfee8bdb4369876ab960eeb6d0be290e0402de96
richardPilonn <richard.pilonzera@gmail.com>
2024-03-08 10:25:02 -0300

    Exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('receber/nome', function(Request $request){
+Route::get('receber/nome', function (Request $request) {
     $nome = $request->input('name');
     return $nome;
+});
+Route::get('naise', function (Request $request) {
+    $nome = $request->input('nome');
+    $idade = $request->input('year');
+
+    return 'Meu nome é ' . $nome . ', tenho ' . $idade . ' anos de idade';
 });
